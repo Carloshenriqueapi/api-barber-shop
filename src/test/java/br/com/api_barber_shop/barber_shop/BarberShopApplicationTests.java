0d284770b419a3e2bddd61df479350618524ed4c
Henrique Bastos <henriquebastos46@gmail.com>
2024-06-11 14:58:19 -0300

    inicializando

---
+++ src/test/java/br/com/api_barber_shop/barber_shop/BarberShopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_barber_shop.barber_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
